5a502cb4928474116a7677c0e53f977f5c2560f1
PaulPrograms <pauldquinones@gmail.com>
2020-04-27 11:38:46 -0500

    Initial commit

---
+++ Assets/Scripts/PetMessageManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PetMessageManager : MonoBehaviour
+{
+    private int currPetStatus = 0;
+    private PetSimulatorManager petSimulatorManager;
+
+    public Text petText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        petSimulatorManager = transform.GetComponent<PetSimulatorManager>();
+        currPetStatus = petSimulatorManager.GetPetStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int petSimStatus = petSimulatorManager.GetPetStatus();
+
+        if (currPetStatus != petSimStatus)
+        {
+            currPetStatus = petSimStatus;
+            SetPetStatus(currPetStatus);
+        }
+    }
+
+    private void SetPetStatus(int status)
+    {
+        switch (status)
+        {
+            case 0:
+                petText.text = "I am Pet.";
+                break;
+            case 1:
+                petText.text = "Pet is hungry.";
+                break;
+            case 2:
+                petText.text = "Pet is sad.";
+                break;
+            case 3:
+                petText.text = "Pet made mess.";
+                break;
+            case 4:
+                petText.text = "Pet angry and bad.";
+                break;
+            default:
+                Debug.Log("Unable to find status");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PetSimulatorManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetSimulatorManager : MonoBehaviour
+{
+    private float currTimer = 0.0f;
+    public float behaviorChangeTime = 30.0f;
+
+    private int status = 0;//0 - fine, 1 - hungry, 2 - sad, 3 - dirty, 4 - bad
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currTimer += Time.deltaTime;
+
+        if(currTimer >= behaviorChangeTime)
+        {
+            ChangeStatus();
+            currTimer = 0.0f;
+        }
+    }
+
+    private void ChangeStatus()
+    {
+        if(status > 0)
+        {
+            status = 0;
+        } 
+        else
+        {
+            status = Random.Range(1, 5);
+        }
+    }
+
+    public void PlayerAction(int type)
+    {
+        if(type == status)
+        {
+            status = 0;
+        }
+        else
+        {
+            status = 4;
+        }
+
+        currTimer = 0.0f;
+    }
+
+    public int GetPetStatus()
+    {
+        return status;
+    }
+}
